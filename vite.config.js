699ea3e6f2fc3392d68f89b0f34a96e36ed8715b
Cody Mims <mims@hey.com>
2022-05-25 16:05:55 -0500

    Fix GitHub Pages build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Stock-Watcher/",
+  base: "/vue-stock-watcher/",
   plugins: [vue()],
   resolve: {
     alias: {
